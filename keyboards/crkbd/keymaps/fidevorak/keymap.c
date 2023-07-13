30d6345aa7f5dca398beb418f29c98598d1a0ec8
Fide Omar Islas <fidev.id@outlook.com>
2023-07-12 23:08:01 -0600

    feat: dvorak and low layer on fidevorak

---
+++ keyboards/crkbd/keymaps/fidevorak/keymap.c
@@ -0,0 +1,207 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+// Initialize variable holding the binary
+// representation of active modifiers.
+uint8_t mod_state;
+
+enum layers { _DVORAK, _LOWER, _RAISE, _ADJUST };
+
+enum my_keycodes { RMT = SAFE_RANGE, RMS, RMIH, RMDH, RMIS, RMDS, RMIV, RMDV };
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // Store the current modifier state in the variable for later reference
+    mod_state = get_mods();
+
+    switch (keycode) {
+        case RMT:
+            if (record->event.pressed) {
+                rgb_matrix_toggle();
+            }
+            return false;
+        case RMS:
+            if (record->event.pressed) {
+                rgb_matrix_step();
+            }
+            return false;
+        case RMIH:
+            if (record->event.pressed) {
+                rgb_matrix_increase_hue();
+            }
+            return false;
+        case RMDH:
+            if (record->event.pressed) {
+                rgb_matrix_decrease_hue();
+            }
+            return false;
+        case RMIS:
+            if (record->event.pressed) {
+                rgb_matrix_increase_sat();
+            }
+            return false;
+        case RMDS:
+            if (record->event.pressed) {
+                rgb_matrix_decrease_sat();
+            }
+            return false;
+        case RMIV:
+            if (record->event.pressed) {
+                rgb_matrix_increase_val();
+            }
+            return false;
+        case RMDV:
+            if (record->event.pressed) {
+                rgb_matrix_decrease_val();
+            }
+            return true;
+        case KC_BSPC: {
+            // Initialize a boolean variable that keeps track
+            // of the delete key status: registered or not?
+            static bool delkey_registered;
+            if (record->event.pressed) {
+                // Detect the activation of either shift keys
+                if (mod_state & MOD_MASK_SHIFT) {
+                    // First temporarily canceling both shifts so that
+                    // shift isn't applied to the KC_DEL keycode
+                    del_mods(MOD_MASK_SHIFT);
+                    register_code(KC_DEL);
+                    // Update the boolean variable to reflect the status of KC_DEL
+                    delkey_registered = true;
+                    // Reapplying modifier state so that the held shift key(s)
+                    // still work even after having tapped the Backspace/Delete key.
+                    set_mods(mod_state);
+                    return false;
+                }
+            } else { // on release of KC_BSPC
+                // In case KC_DEL is still being sent even after the release of KC_BSPC
+                if (delkey_registered) {
+                    unregister_code(KC_DEL);
+                    delkey_registered = false;
+                    return false;
+                }
+            }
+            // Let QMK process the KC_BSPC keycode as usual outside of shift
+            return true;
+        }
+        case LT(_DVORAK, KC_SCLN): {
+            if (record->tap.count && record->event.pressed) {
+                return true; // Return true for normal processing of tap keycode
+                break;
+            } else if (record->event.pressed) {
+                tap_code16(KC_COLN); // Intercept hold function to send COLON
+                return false;
+            }
+            return true; // this allows for normal processing of key release!
+        }
+        case LT(_DVORAK, KC_N): {
+            if (record->tap.count && record->event.pressed) {
+                return true; // Return true for normal processing of tap keycode
+                break;
+            } else if (record->event.pressed) {
+                // Intercept hold function to send ñ/Ñ
+                register_code(KC_ALGR);
+                tap_code16(KC_N);
+                unregister_code(KC_ALGR);
+                return false;
+            }
+            return true; // this allows for normal processing of key release!
+        }
+
+        default:
+            return true;
+    }
+}
+
+#define LOWER LT(_LOWER, KC_SPC)
+#define RAISE LT(_RAISE, KC_ENT)
+#define LSFTCAP SFT_T(KC_CAPS)
+#define SEMI_CLN LT(_DVORAK, KC_SCLN)
+#define CUSTOM_N LT(_DVORAK, KC_N)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* DVORAK
+ * +------+------+------+------+------+------+                 +------+------+------+------+------+------+
+ * | Esc  |   '  |   ,  |   .  |   P  |   Y  |                 |   F  |   G  |   C  |   R  |   L  | Bksp |
+ * +------+------+------+------+------+------+                 +------+------+------+------+------+------+
+ * | Tab  |   A  |   O  |   U  |   I  |   D  |                 |   D  |   H  |   T  |   N  |   S  |   /  |
+ * +------+------+------+------+------+------+                 +------+------+------+------+------+------+
+ * |Shift |   :  |   Q  |   J  |   K  |   X  +------+   +------+   B  |   M  |   W  |   V  |   Z  |   -  |
+ * +------+------+------+------+------+------+ Lower|   |Raise +------+------+------+------+------+------+
+ *                             | Ctrl |Super |      |   |      |  Alt |      |
+ *                             +------+------+------+   +------+------+------+   
+ */
+  [_DVORAK] = LAYOUT_split_3x6_3(
+//+--------+--------+--------+--------+--------+--------+                    +--------+--------+--------+--------+--------+--------+
+     KC_ESC, KC_QUOT, KC_COMM,  KC_DOT,    KC_P,    KC_Y,                         KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_BSPC,
+//+--------+--------+--------+--------+--------+--------+                    +--------+--------+--------+--------+--------+--------+
+     KC_TAB,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                         KC_D,    KC_H,    KC_T,CUSTOM_N,    KC_S, KC_SLSH,
+//+--------+--------+--------+--------+--------+--------+                    +--------+--------+--------+--------+--------+--------+
+    LSFTCAP,SEMI_CLN,    KC_Q,    KC_J,    KC_K,    KC_X,                         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_MINS,
+//+--------+--------+--------+--------+--------+--------+--------+  +--------+--------+--------+--------+--------+--------+--------+
+                                        KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_RALT
+                                    //+--------+--------+--------+  +--------+--------+--------+
+  ),
+
+/* Lower
+ * +------+------+------+------+------+------+                 +------+------+------+------+------+------+
+ * |      |      |      |      |      |      |                 |      |   7  |   8  |   9  |   =  | Bksp |
+ * +------+------+------+------+------+------+                 +------+------+------+------+------+------+
+ * |      |      |      |      |      |      |                 |      |   4  |   5  |   6  |   *  |   /  |
+ * +------+------+------+------+------+------+                 +------+------+------+------+------+------+
+ * |      |      |      |      |      |      +------+   +------+      |   1  |   2  |   3  |   +  |   -  |
+ * +------+------+------+------+------+------+      |   |      +------+------+------+------+------+------+
+ *                             |      |      |      |   |      |      |   0  |
+ *                             +------+------+------+   +------+------+------+   
+ */
+  [_LOWER] = LAYOUT_split_3x6_3(
+//+--------+--------+--------+--------+--------+--------+                    +--------+--------+--------+--------+--------+--------+
+    _______, _______, _______, _______, _______, _______,                      _______,    KC_7,    KC_8,    KC_9,  KC_EQL, _______,
+//+--------+--------+--------+--------+--------+--------+                    +--------+--------+--------+--------+--------+--------+
+    _______, _______, _______, _______, _______, _______,                      _______,    KC_4,    KC_5,    KC_6, KC_ASTR, _______,
+//+--------+--------+--------+--------+--------+--------+                    +--------+--------+--------+--------+--------+--------+
+    _______, _______, _______, _______, _______, _______,                      _______,    KC_1,    KC_2,    KC_3, KC_PLUS, _______,
+//+--------+--------+--------+--------+--------+--------+--------+  +--------+--------+--------+--------+--------+--------+--------+
+                                        _______, _______, _______,    _______, _______,    KC_0
+                                    //+--------+--------+--------+  +--------+--------+--------+
+  ),
+
+    [2] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+    [3] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+                                      //`--------------------------'  `--------------------------'
+  )
+};
